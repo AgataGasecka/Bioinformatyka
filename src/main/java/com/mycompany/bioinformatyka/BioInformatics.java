98dbb63efd7a1037e27caaccfc8eb1985dca5aa2
AgataGasecka <gaseckaagata@gmail.com>
2018-12-15 15:58:29 +0100

    nowy projekt

---
+++ src/main/java/com/mycompany/bioinformatyka/BioInformatics.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+package com.mycompany.bioinformatyka;
+
+import javax.swing.text.StyledEditorKit;
+import org.biojava.nbio.core.alignment.SimpleAlignedSequence;
+import org.biojava.nbio.alignment.Alignments;
+/**
+ *
+ * @author Agata
+ */
+public class BioInformatics {
+    
+    
+    public static void main(String[] args) throws Exception {
+        System.out.println("Agata");
+        
+        
+        
+    }
+}
